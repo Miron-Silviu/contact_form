a1a89beecf776a236ebea292c90b651d1eaf25a3
Silviu <silviu_miron_90@outlook.com>
2025-05-26 22:10:31 +0100

    Fix : Remove tailwind.css

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-// tailwind.config.js
-module.exports = {
-  content: ['./src/**/*.{html,js}'], // adjust to your actual html/js file locations
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
